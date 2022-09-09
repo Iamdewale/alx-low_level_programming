a8fa40973850a538a11a51614dacddae8b31af47
iamdewale <dewalefams@gmail.com>
2022-09-09 16:56:25 -0700

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,13 +10,13 @@ int main(void)
 	int tens;
 	int ones;
 
-	for (tens = '0'; tens <= '9'; tens++) /*print tens place*/
+	for (tens = '0'; tens <= '9'; tens++) 
 	{
-		for (ones = '0'; ones <= '9'; ones++) /*print ones place*/
+		for (ones = '0'; ones <= '9'; ones++) 
 		{
 			putchar(tens);
 			putchar(ones);
-			if (!(tens == '9' && ones == '9')) /*skip comma at end*/
+			if (!(tens == '9' && ones == '9'))
 			{
 				putchar(',');
 				putchar(' ');
