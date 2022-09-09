949c7de6532743139330610a8e11bf92adefb97a
iamdewale <dewalefams@gmail.com>
2022-09-09 16:04:46 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print 0123456789
+ * return 0 (success)
+ */
+
+int main(void)
+{
+	int x = 0;
+
+	while (x < 10)
+	{
+		printf("%i", x);
+		x++;
+	}
+	putchar('\n');
+
+	return (0);
+}
