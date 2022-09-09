9062fc84888a469201cf7b40785959e8621372e7
iamdewale <dewalefams@gmail.com>
2022-09-09 14:34:50 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#iinclude <stdio>
+
+/**
+ * main - print lower and upper using putcha
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char lower = 'a';
+	char upper = 'A';
+
+	while (lower <= 'z')
+	{
+		putchar(lower);
+		lower++;
+	}
+
+	while (upper <= 'z')
+	{
+		putchar(upper);
+		upper++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
