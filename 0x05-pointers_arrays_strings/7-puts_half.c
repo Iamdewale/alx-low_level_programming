59bb5710f655db72e105c8b1bf8bfac3e4ed3d21
iamdewale <dewalefams@gmail.com>
2022-09-19 11:51:35 -0700

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,35 +1,23 @@
 #include "main.h"
 
 /**
- * _strlen - returns the length of a string
- * @s: string
- * Return: length
+ * puts_half - print second half of a string
+ * @str: char array string type
+ * Description: If odd number of chars.
  */
 
-int _strlen(char *s)
-{
-	int len = 0;
-
-	while (*s != '\0')
-	{
-		len++;
-		s++;
-	}
-
-	return (len);
-}
-
-/**
- * puts2 - prints every second character of a string, followed by a new line
- * @str: string to print
- */
-
-void puts2(char *str)
+void puts_half(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0' && i < _strlen(str); i += 2)
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	i++;
+	for (i /= 2; str[i] != '\0'; i++)
+	{
 		_putchar(str[i]);
 
+	}
 	_putchar('\n');
 }
