9650ad2c7b9f414ea82fdc2a6339b6268a842fd0
iamdewale <dewalefams@gmail.com>
2022-09-22 12:52:33 -0700

    task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,6 +10,22 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
+}
+
+/*
+char *_strncpy(char *dest, char *src, int n)
+{
   int i;
   for (i = 0; i < n && *(src + i); i++)
       *(dest + i) = *(src + i);
@@ -20,3 +36,4 @@ char *_strncpy(char *dest, char *src, int n)
   }
   return (dest);
 }
+*/
