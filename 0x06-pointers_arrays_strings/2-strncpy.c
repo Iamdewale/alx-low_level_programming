ad28827d586aca46514e1f8cf2b52cad9017a60f
iamdewale <dewalefams@gmail.com>
2022-09-22 12:57:15 -0700

    task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,6 +5,9 @@
  * @dest: copy source to this buffer
  * @src: this is the source to copy
  * @n: n bytes to be copied
+ * at mst n bytes of src are copied
+ * if length of src is < n, additional null bytes are written to dest
+ * to ensure that a total of n bytes are written.
  * Return: copied string
  */
 
